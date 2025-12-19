88a55b4e42f6a2deeda7866872398523a52aad0f
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-19 19:35:41 +0100

    Update ornaments to be emojis

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ const month = date.getMonth() + 1;
 const year = date.getFullYear();
 
 //list of all the images in img/ornaments and a list of emojis the top decoration thing can be
-const ornamentImg = [
-	"1.webp",
-	"2.gif",
-	"3.png",
-	"4.png",
-	"5.png",
-	"6.png",
-	"7.webp",
-	"8.png",
-	"9.png",
-	"10.png",
-	"11.png",
-	"12.png",
-	"13.png",
-	"14.jpg",
-	"15.gif",
-	"16.png",
-	"17.png",
-	"18.png",
+const ornamentItems = [
+	"🎄",
+	"🎅",
+	"🤶",
+	"🎁",
+	"⭐",
+	"❄️",
+	"☃️",
+	"🦌",
+	"🔔",
+	"🎀",
+	"🍪",
+	"🥛",
+	"🧦",
+	"🕯️",
+	"✨",
+	"💫",
+	"🍬",
+	"🎉",
 ];
 const stars = ["⭐", "💫", "💖", "🎄", "🏴‍☠️", "👾"];
 
@@ -48,8 +48,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		//for each day til christmas; it adds another ornament to the christmas tree
 		for (let i = 0; i < daysTilChristmas; i++) {
-			const randomImg = ornamentImg[Math.floor(Math.random() * 18)];
-			decorations.innerHTML += `<img src="img/ornaments/${randomImg}" class="ornament" id="ornament${i}">`;
+			const randomImg = ornamentItems[Math.floor(Math.random() * 18)];
+			decorations.innerHTML += `<p class="ornament" id="ornament${i}">${randomImg}</p>`;
 			const ornament = document.getElementById("ornament" + i);
 			//sets the margins to a random value between (4,3) and (14,5)
 			ornament.style.marginLeft = Math.random() * 10 + 4 + "rem";
